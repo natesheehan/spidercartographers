49c115a5b36f6df7e97a9850a68b0231514d7eaa
alicjakot <alicjakotarbaaa@gmail.com>
2020-04-16 13:32:23 +0100

    story page changes with loads of problems

--- assets/scripts/parallax.js
+++ assets/scripts/parallax.js
@@ -10,4 +10,38 @@ jQuery(document).ready(function(){
    $('#parallax-bg-3').css('top',(0-(scrolled*.75))+'px');
  }
 
+  //  displaying increasing numbers function
+  $('.percText').counterUp({
+    delay: 10,
+    time: 1000
+  });
+
+  //variety chart
+  const margin = 60;
+  const width = 680;
+  const height = 400;
+
+  const svg = d3.select('svg');
+  const chart = svg.append('g')
+    .attr('transform',`translate(${margin}, ${margin})`);
+
+  const yScale = d3.scaleLinear()
+    .range([height, 0])
+    .domain([0, 100]);
+  
+  chart.append('g')
+    .call(d3.axisLeft(yScale));
+
+  const xScale = d3.scaleBand()
+    .range([0, width])
+    .domain(['a','b','c'].map((s)=>s))
+    .padding(0.2)
+
+  chart.append('g')
+    .attr('transform',`translate(0, ${height})`)
+    .call(d3.axisBottom(xScale));
+
+  
+
+
 });
