f0b84b905ff82a042a25ceee44ce30bbfe526dcb
alicjakot <alicjakotarbaaa@gmail.com>
2020-05-06 16:15:25 +0100

    sharing it here guys so you can work on it too

--- assets/scripts/map.js
+++ assets/scripts/map.js
@@ -46,11 +46,25 @@ var msoaData = $.ajax({
     // Check that data was read in correctly 
     console.log(msoas);
 
-    // attempt to color clusters using array 
-/*  async function findClusters() {
+    /////////////////////////////////// CLUSTER LOOK UP ARRAY //////////////////////////////
+    // Check that it worked 
+    // console.log(msoas);
+
+    const msoasPart1 = {
+      "type": "FeatureCollection",
+      "name": "d443bed3-24e4-4541-a1d7-0657a0dfa8492020329-1-16kypme.674w",
+      "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+      "features": []};
+    const msoasPart2 ={
+      "type": "FeatureCollection",
+      "name": "d443bed3-24e4-4541-a1d7-0657a0dfa8492020329-1-16kypme.674w",
+      "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+      "features": []};
+
+    async function findClusters() {
       // Create array for looking up clusters
       var clusterLookUp = {}; 
-    
+      
       msoas.features.forEach(entry => {
           
           // Get MSOA ID of feature
@@ -62,14 +76,31 @@ var msoaData = $.ajax({
 
           msoaRef.once('value', function(data) {
               var clusterID = data.val().log_zscore_kmeans_cluster; 
-              // clusterLookUp[msoaID] = clusterID;
+              clusterLookUp[msoaID] = clusterID;
               entry.properties = {...entry.properties, clusterID: clusterID} //that adds another property of clusterID to msoas
-              // console.log(clusterLookUp);
-          });
-    
+              console.log(clusterLookUp);
+          });       
       })
-      // console.log(msoas)
-    } */
+
+
+      for (var i=0; i<msoas.features.length; i++){
+        if (i<4000){
+          msoasPart1.features.push(msoas.features[i]);
+        }
+        else{
+          msoasPart2.features.push(msoas.features[i]);
+        }
+      }
+
+      console.log(msoasPart2)
+      console.log(msoas)
+
+    }
+   
+    /////////////////////////////////// ^ CLUSTER LOOK UP ARRAY ^ //////////////////////////////
+  
+    findClusters();
+
 
     map.on('load', function() {
 
@@ -77,7 +108,8 @@ var msoaData = $.ajax({
         map.addSource('states', {
             'type': 'geojson',
             'data': msoas,
-            'generateId': true 
+            'buffer': 1,
+            'generateId': true // This ensures that all features have unique IDs
         });
 
         // Add centroids 
@@ -93,27 +125,24 @@ var msoaData = $.ajax({
             'source': 'states',
             'layout': {},
             'paint': {
-              'fill-color': '#627BC1',
-              'fill-opacity': [
-                  'case',
-                  ['boolean', ['feature-state', 'hover'], false],
-                  1,
-                  0.5
+            'fill-color': {
+              property: 'clusterID',
+              stops: [
+                  [1, 'white'],
+                  [2, 'red'],
+                  [3, 'blue'],
+                  [4, 'green'],
+                  [5, 'yellow']                  
               ]
-            }
-        });
-
-            // potential cluster colors solution
-/*             'fill-color': {
-              'property': 'clusterID',
-              'stops': [
-                [1, 'white'],
-                [2, 'orange'],
-                [3, 'firebrick'],
-                [4, 'blue'],
-                [0, 'grey']
-              ]
-            },
+          },
+            // ['match',
+            //   ['get', 'clusterID'],
+            //   1,'blue',
+            //   2,'red',
+            //   3,'green',
+            //   4,'yellow',
+            //   5,'pink',
+            //   /* other */ 'black'],
             'fill-opacity': [
                 'case',
                 ['boolean', ['feature-state', 'hover'], false],
@@ -121,20 +150,20 @@ var msoaData = $.ajax({
                 0.5
                 ]
             }
-        }); */
+        }); 
 
-       // another attempt to do cluster colors with choropleth
-/*         let c = new MapboxChoropleth({
-            tableUrl: 'http://127.0.0.1:8887/data/test.csv',
-            tableNumericField: 'cluster',
-            tableIdField: 'msoa',
-            geometryUrl: 'https://opendata.arcgis.com/datasets/29fdaa2efced40378ce8173b411aeb0e_2.geojson',
-            geometryIdField: 'msoa11cd',
-            sourceLayer: 'state-fills',
-            binCount: 5,
-            colorScheme: 'Spectral',
-            legendElement: '#legend'
-        }).addTo(map); */
+       // another attempt with choropleth
+        // let c = new MapboxChoropleth({
+        //     tableUrl: 'http://127.0.0.1:8887/data/test.csv',
+        //     tableNumericField: 'cluster',
+        //     tableIdField: 'msoa',
+        //     geometryUrl: 'https://opendata.arcgis.com/datasets/29fdaa2efced40378ce8173b411aeb0e_2.geojson',
+        //     geometryIdField: 'msoa11cd',
+        //     sourceLayer: 'state-fills',
+        //     binCount: 5,
+        //     colorScheme: 'Spectral',
+        //     legendElement: '#legend'
+        // }).addTo(map);
 
           // Add MSOA outlines         
           map.addLayer({
@@ -144,7 +173,7 @@ var msoaData = $.ajax({
             'layout': {},
             'paint': {
                 'line-color': '#627BC1',
-                'line-width': 2
+                'line-width': 0.5
             }
         });
 
@@ -152,6 +181,64 @@ var msoaData = $.ajax({
         var idDisplay = document.getElementById('msoaId');
         var nameDisplay = document.getElementById('msoaName');
         var clusterNumberDisplay = document.getElementById('clusterNumber');
+        var msoasID = null;
+
+        // map.on('click', 'state-fills', (e) => {
+
+        // });
+
+        map.on('mousemove', 'state-fills', (e) => {
+
+        map.getCanvas().style.cursor = 'pointer';
+        // Set variables equal to the current feature's id and name
+        var idOfMsoa = e.features[0].properties.objectid;
+        var nameOfMsoa = e.features[0].properties.msoa11nm;
+        var ref = db.ref(e.features[0].properties.msoa11cd);
+
+        ref.on('value', getData, errData);
+
+        var currentObj;
+        function getData(data){
+          currentObj = data.val();
+        }
+
+        function errData(data){
+          console.log("ERROR");
+          console.log(err);
+        }
+        // console.log(currentObj)
+
+
+        // Check whether features exist
+        if (e.features.length > 0) {
+            // Display the id, name and cluster in the sidebar
+            idDisplay.textContent = idOfMsoa;
+            nameDisplay.textContent = nameOfMsoa;
+            clusterNumberDisplay.textContent = currentObj.log_zscore_kmeans_cluster;
+            // bikeUsageDisplay.textContent = currentObj.bicycle_perc;
+            console.log(currentObj);
+
+            //data for the chart
+            const a = currentObj.work_from_home_perc;
+            const b = currentObj.on_foot_perc;
+            const c = currentObj.bicycle_perc;
+            const d = currentObj.car_perc;
+            const e = currentObj.bus_perc;
+            const f = currentObj.train_perc;
+            const g = currentObj.underground_metro_perc;
+            drawChart(a,b,c,d,e,f,g)
+
+
+            // If quakeID for the hovered feature is not null,
+            // use removeFeatureState to reset to the default behavior
+            if (msoasID) {
+            map.removeFeatureState({
+                source: "states",
+                id: msoasID
+            });
+            }
+
+            msoasID = e.features[0].id;
 
         ///////////////////////////////////////////////////////////////////////////
          
@@ -267,5 +354,6 @@ var msoaData = $.ajax({
             }
         }); // end of flows switch
 
-    }); // end of on load 
-  }); // end of ajax
+    }}) // end of on load 
+  })// end of ajax
+  });
