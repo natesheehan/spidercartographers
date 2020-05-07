4543274915adf55a2a0d12de66b8243d3dfef0f0
Nate <nathanaelsheehan@gmail.com>
2020-05-07 16:47:53 +0100

    Added lost file (NS)

---
+++ assets/scripts/peopleAndPublicTransport.js
@@ -0,0 +1,58 @@
+var DEFAULT_DATASET_SIZE = 6,
+             addedCount = 0,
+              color = Chart.helpers.color;
+
+         var chartColors = {
+           orange: '#ffa372',
+           black: '#1b262c',
+           grey: '#bdccd4'
+         };
+
+       var barPeopleData = {
+             labels: ["MIN","25%","50%","75%","MAX", "MEAN"],
+             datasets: [{
+               label: 'People Power',
+               backgroundColor: color(chartColors.grey).alpha(0.5).rgbString(),
+               borderColor: chartColors.grey,
+               borderWidth: 1,
+               data: [
+                           2.019363762,
+                           7.076739325,
+                           10.28142589,
+                           15.51043429,
+                           53.8021978,
+                           12.44920972
+               ]
+             },{
+               label: 'Public Transport',
+               backgroundColor: color(chartColors.orange).alpha(0.5).rgbString(),
+               borderColor: chartColors.orange,
+               borderWidth: 1,
+               data: [
+                           0.744047619,
+                           5.615874204,
+                           9.99275887,
+                           18.146184,
+                           74.62533361,
+                           15.38149353
+               ]
+             }]
+
+           };
+       var index = 11;
+       var ctx = document.getElementById("barChartPeople").getContext("2d");
+           var	myNewChartB = new Chart(ctx, {
+               type: 'bar',
+               data: barPeopleData,
+               options: {
+                 responsive: true,
+                 maintainAspectRation: true,
+                 legend: {
+                   position: 'top',
+                 },
+                 title: {
+                   display: true,
+                   text: 'Public Transport'
+                 }
+               }
+             });
