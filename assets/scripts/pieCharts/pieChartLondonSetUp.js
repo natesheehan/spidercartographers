adcda09510bc9fef26f16af3cf985a7d7a897818
Nate <nathanaelsheehan@gmail.com>
2020-05-14 15:11:33 +0100

    big chunky code

---
+++ assets/scripts/pieCharts/pieChartLondonSetUp.js
@@ -0,0 +1,5 @@
+d3.json("http://dev.spatialdatacapture.org:8717/cityscale/mean/london", function(urbanData) {
+    var urbanMeanData = urbanData;
+    var colorScheme = ["#ffa372"," #ff059","#ed6663","#bdccd4","#37a583","#186da0","#1b262c","#4DB6AC","#FFF176","#64B5F6","#00E676","#00008b"];
+    renderLondonPieChart(urbanMeanData,"#chartLondon",colorScheme);
+});
