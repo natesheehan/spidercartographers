e61d8536e1701de52c86aa920fd8981edf63de44
Nate <nathanaelsheehan@gmail.com>
2020-05-19 14:19:44 +0100

    WIP

--- assets/scripts/clusterGraphs.js
+++ assets/scripts/clusterGraphs.js
@@ -1,16 +1,21 @@
 // set the dimensions and margins of the graph
-var margin = {top: 10, right: 30, bottom: 20, left: 50},
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+var marginBarCar = {
+    top: 20,
+    right: 30,
+    bottom: 40,
+    left: 90
+  },
+  widthBarCar = 460 - marginBarCar.left - marginBarCar.right,
+  heightBarCar = 400 - marginBarCar.top - marginBarCar.bottom;
 
 // append the svg object to the body of the page
-var svg = d3.select("#my_dataviz")
+var svgBarCar = d3.select("#barCar")
   .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
+  .attr("width", widthBarCar + marginBarCar.left + marginBarCar.right)
+  .attr("height", heightBarCar + marginBarCar.top + marginBarCar.bottom)
   .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
+  .attr("transform",
+    "translate(" + marginBarCar.left + "," + marginBarCar.top + ")"
 
 d3.json("http://dev.spatialdatacapture.org:8717/data/clusters/average", function(data) {
 
@@ -25,7 +30,9 @@ delete data[0].on_foot_perc
 delete data[0].train_perc
 delete data[0].underground_metro_perc
 delete data[0].work_from_home_perc
-data[0].cluster label = "clusterID"
+delete data[0].avg_time_from_origin_rail_UNWEIGHTED
+delete data[0].avg_time_from_origin_bus_UNWEIGHTED
+
 
 delete data[1].HH_owning_cars_perc
 delete data[1].avg_time_bus
@@ -38,7 +45,8 @@ delete data[1].on_foot_perc
 delete data[1].train_perc
 delete data[1].underground_metro_perc
 delete data[1].work_from_home_perc
-data[1].cluster label = "clusterID"
+delete data[1].avg_time_from_origin_rail_UNWEIGHTED
+delete data[1].avg_time_from_origin_bus_UNWEIGHTED
 
 delete data[2].HH_owning_cars_perc
 delete data[2].avg_time_bus
@@ -51,7 +59,8 @@ delete data[2].on_foot_perc
 delete data[2].train_perc
 delete data[2].underground_metro_perc
 delete data[2].work_from_home_perc
-data[2].cluster label = "clusterID"
+delete data[2].avg_time_from_origin_rail_UNWEIGHTED
+delete data[2].avg_time_from_origin_bus_UNWEIGHTED
 
 delete data[3].HH_owning_cars_perc
 delete data[3].avg_time_bus
@@ -64,7 +73,8 @@ delete data[3].on_foot_perc
 delete data[3].train_perc
 delete data[3].underground_metro_perc
 delete data[3].work_from_home_perc
-data[3].cluster label = "clusterID"
+delete data[3].avg_time_from_origin_rail_UNWEIGHTED
+delete data[3].avg_time_from_origin_bus_UNWEIGHTED
 
 delete data[4].HH_owning_cars_perc
 delete data[4].avg_time_bus
@@ -76,57 +86,62 @@ delete data[4].car_perc
 delete data[4].on_foot_perc
 delete data[4].train_perc
 delete data[4].underground_metro_perc
-delete data[4].work_from_home_perc
-data[4].cluster label = "clusterID"
-
+delete data[4].work_from_home_percs
+delete data[4].avg_time_from_origin_rail_UNWEIGHTED
+delete data[4].avg_time_from_origin_bus_UNWEIGHTED
 
 console.log(data)
 
-
-// List of groups = species here = value of the first column called group -> I show them on the X axis
-var groups = d3.map(data, function(d){return(d.cluster label)}).keys()
-
 // Add X axis
-var x = d3.scaleBand()
-    .domain(groups)
-    .range([0, width])
-    .padding([0.2])
-svg.append("g")
-  .attr("transform", "translate(0," + height + ")")
-  .call(d3.axisBottom(x).tickSize(0));
-
-// Add Y axis
-var y = d3.scaleLinear()
+var x = d3.scaleLinear()
   .domain([0, 40])
-  .range([ height, 0 ]);
-svg.append("g")
-  .call(d3.axisLeft(y));
+  .range([0, widthBarCar]);
+svgBar.append("g")
+  .attr("transform", "translate(0," + heightBarCar + ")")
+  .call(d3.axisBottom(x))
+  .selectAll("text")
+  .attr("transform", "translate(-10,0)rotate(-45)")
+  .style("text-anchor", "end");
 
-// Another scale for subgroup position?
-var xSubgroup = d3.scaleBand()
-  .domain(subgroups)
-  .range([0, x.bandwidth()])
-  .padding([0.05])
+// Y axis
+var y = d3.scaleBand()
+  .range([0, heightBarCar])
+  .domain(data.map(function(d) {
+    return d.mode;
+  }))
+  .padding(.1);
+svgBarCar.append("g")
+  .call(d3.axisLeft(y))
 
-// color palette = one color per subgroup
-var color = d3.scaleOrdinal()
-  .domain(subgroups)
-  .range(['#e41a1c','#377eb8','#4daf4a'])
-
-// Show the bars
-svg.append("g")
-  .selectAll("g")
-  // Enter in data = loop group per group
+//Bars
+svgBarCar.selectAll("myRect")
   .data(data)
   .enter()
-  .append("g")
-    .attr("transform", function(d) { return "translate(" + x(d.clusterID) + ",0)"; })
-  .selectAll("rect")
-  .data(function(d) { return subgroups.map(function(key) { return {key: key, value: d[key]}; }); })
-  .enter().append("rect")
-    .attr("x", function(d) { return xSubgroup(d.key); })
-    .attr("y", function(d) { return y(d.value); })
-    .attr("width", xSubgroup.bandwidth())
-    .attr("height", function(d) { return height - y(d.value); })
-    .attr("fill", function(d) { return color(d.key); });
+  .append("rect")
+  .attr("x", x(0))
+  .attr("y", function(d) {
+    return y(d.mode);
+  })
+  .attr("width", function(d) {
+    return x(d.["cluster label"]);
+  })
+  .attr("height", y.bandwidth())
+  .attr("fill", "#37a583")
+
+  svgBarCar.append("text")
+    .attr("class", "x label")
+    .attr("text-anchor", "end")
+    .attr("x", widthBarCar)
+    .attr("y", heightBarCar +35)
+    .text("Mean Public Transport Access in MSOA's");
+
+    svgBarCar.append("text")
+    .attr("class", "y label")
+    .attr("text-anchor", "end")
+    .attr("y", -10)
+    .attr("x", 5)
+    .attr("dy", ".75em")
+    .attr("transform", "rotate(-360)")
+    .text("Transport Type");
+
 })
